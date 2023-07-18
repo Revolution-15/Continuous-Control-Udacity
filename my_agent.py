a13b9f214a935b6d1f46069649c7fafe70aefbcd
Revolution-15 <sushantsuryawanshi7@gmail.com>
2023-07-19 00:34:12 +0530

    Add files via upload

--- my_agent.py
+++ my_agent.py
@@ -3,7 +3,7 @@ import random
 import copy
 from collections import namedtuple, deque
 
-from model_pendulum import Actor, Critic
+from my_model import Actor, Critic
 
 import torch
 import torch.nn.functional as F
